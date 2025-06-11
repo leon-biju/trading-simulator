54f899bdc5702227502e34c41d901d3349745fd2
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-06-11 20:09:40 +0100

    feature/accounts: create profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,16 @@
+from decimal import Decimal
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth.models import User
-from decimal import Decimal
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cash_balance = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal('100000.00'))
-    created_at = models.DateTimeField(auto_now_add=True)
+    """
+    Non-auth data
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    cash_balance = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal("100000.00"))
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}: £{self.cash_balance}"
+        return f"Profile of {self.user.username}"
--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
