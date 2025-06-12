5e11a2e5468d1fea7dd45cd88b88575798af883f
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-06-12 23:58:39 +0100

    feature/accounts: fix profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
+# For registering models that show up in  admin page
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cash_balance', 'created_on')
+    search_fields = ('user__username',)
--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,6 @@ from django.db import models
 
 
 class Profile(models.Model):
-    """
-    Non-auth data
-    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     cash_balance = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal("100000.00"))
     created_on = models.DateTimeField(auto_now_add=True)
