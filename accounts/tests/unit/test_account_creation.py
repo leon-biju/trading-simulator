2c252b2e69e372abf982eeb58399f18ce43299b3
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-07-02 20:29:44 +0100

    add email normalisation unit test

---
+++ accounts/tests/unit/test_account_creation.py
@@ -0,0 +1,23 @@
+import pytest
+
+from accounts.forms import SignUpForm
+
+
+# Test email normalisation
+@pytest.mark.parametrize(
+    "raw, expected",
+    [
+        ("FOO@EXAMPLE.COM", "foo@example.com"),
+        ("MiXeD@Case.Org", "mixed@case.org"),
+        ("already.lower@ed.net", "already.lower@ed.net"),
+    ],
+)
+def test_normalised_email(raw, expected):
+    """
+    SignUpForm.clean_email() must always return a lowercased email since
+    emails are case insensitive
+    """
+    
+    form = SignUpForm()
+    form.cleaned_data = {"email": raw}
+    assert form.clean_email() == expected 
