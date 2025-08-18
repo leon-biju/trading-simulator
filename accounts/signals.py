015229983d9f8b5e5b2bd51c52e0545841d136cf
Leon Biju <31255961+leon-biju@users.noreply.github.com>
2025-08-18 16:17:29 +0100

    fix profile signal bug

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Profile
+from django.conf import settings
 
 # Create a profile for a new instance of User 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
