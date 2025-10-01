37be78262cb8be9bea425b8ae8b6b121c9f6a9b0
Leon Biju <leonbiju25@gmail.com>
2025-10-01 17:44:18 +0100

    rename core -> config

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -87,7 +87,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
