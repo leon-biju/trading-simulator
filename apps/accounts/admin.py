f7d39ab302366a244ccc0fbcfdffc4cc2ea2adb9
Leon Biju <leonbiju25@gmail.com>
2025-10-08 12:59:22 +0100

    add user table to admin page

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,9 +1,14 @@
 # For registering models that show up in  admin page
 from django.contrib import admin
-from .models import Profile
+from .models import CustomUser, Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'created_on')
     search_fields = ('user__username',)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email')
