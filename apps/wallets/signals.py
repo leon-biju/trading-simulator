079ee5c947187aed8ecbb02ee3a88b8a0c2a24e0
Leon Biju <leonbiju25@gmail.com>
2025-10-23 20:52:39 +0100

    create all 3 wallets upon user creation

--- apps/wallets/signals.py
+++ apps/wallets/signals.py
@@ -2,10 +2,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Wallet
 from django.conf import settings
+from .models import Currency
 
 # Create a wallet for a new instance of User 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_wallet(sender, instance, created, **kwargs):
+def create_wallets(sender, instance, created, **kwargs):
     if created:
-        Wallet.objects.create(user=instance, balance=settings.STARTING_BALANCE)
-        # Only new users get a wallet created loaded with 100,000 gbp
+        Wallet.objects.bulk_create(
+            [Wallet(user=instance, currency=cur.value, balance=0) for cur in Currency]
+        )
+        # Set gbp wallet balance to £100,000
+        wallet = Wallet.objects.get(user=instance, currency='GBP')
+        wallet.balance = 100000
+        wallet.save()
