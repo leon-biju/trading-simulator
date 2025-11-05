1b5381d4ba34ef18d8056d88b989434a21813de9
Leon Biju <leonbiju25@gmail.com>
2025-11-05 13:42:34 +0000

    register trading + market models in admin page

--- apps/trading/admin.py
+++ apps/trading/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.trading.models import Portfolio, Order, Fill, CommissionRule
 
-# Register your models here.
+admin.site.register(Portfolio)
+admin.site.register(Order)
+admin.site.register(Fill)
+admin.site.register(CommissionRule)
