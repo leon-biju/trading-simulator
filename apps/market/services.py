3d4293548fd4136f77440130c498193dcd92f5d1
Leon Biju <leonbiju25@gmail.com>
2025-11-13 16:44:22 +0000

    fix stock currency bug when creating

--- apps/market/services.py
+++ apps/market/services.py
@@ -2,13 +2,12 @@ from django.db import transaction
 from .models import Currency, Stock, CurrencyAsset, Exchange
 
 @transaction.atomic
-def create_stock_asset(symbol: str, name: str, exchange: Exchange) -> Stock:
-    base_currency = Currency.objects.get(is_base=True)
+def create_stock_asset(symbol: str, name: str, exchange: Exchange, currency: Currency) -> Stock:
     stock = Stock.objects.create(
         asset_type='STOCK',
         symbol=symbol,
         name=name,
-        currency=base_currency,
+        currency=currency,
         exchange=exchange
     )
     return stock
@@ -17,7 +16,7 @@ def create_stock_asset(symbol: str, name: str, exchange: Exchange) -> Stock:
 @transaction.atomic
 def create_currency_asset(symbol: str, name: str) -> CurrencyAsset:
     #All currency assets are priced in the base currency.
-    
+
     base_currency = Currency.objects.get(is_base=True)
     currency_asset = CurrencyAsset.objects.create(
         asset_type='CURRENCY',
