bff8134e3c967afbefb2484f884a4f727a0e6cf8
Leon Biju <leonbiju25@gmail.com>
2025-11-13 16:32:04 +0000

    move creation of assets to services

---
+++ apps/market/services.py
@@ -0,0 +1,28 @@
+from django.db import transaction
+from .models import Currency, Stock, CurrencyAsset, Exchange
+
+@transaction.atomic
+def create_stock_asset(symbol: str, name: str, exchange: Exchange) -> Stock:
+    base_currency = Currency.objects.get(is_base=True)
+    stock = Stock.objects.create(
+        asset_type='STOCK',
+        symbol=symbol,
+        name=name,
+        currency=base_currency,
+        exchange=exchange
+    )
+    return stock
+
+
+@transaction.atomic
+def create_currency_asset(symbol: str, name: str) -> CurrencyAsset:
+    #All currency assets are priced in the base currency.
+    
+    base_currency = Currency.objects.get(is_base=True)
+    currency_asset = CurrencyAsset.objects.create(
+        asset_type='CURRENCY',
+        symbol=symbol,
+        name=name,
+        currency=base_currency
+    )
+    return currency_asset
