e0ad746ac900e94cfc02fe28c60f6b3235d26354
Leon Biju <leonbiju25@gmail.com>
2025-11-17 20:22:13 +0000

    remove redundant class for simulations

--- apps/market/simulation.py
+++ apps/market/simulation.py
@@ -12,40 +12,40 @@ SIGMA = 0.20
 # We use this to scale our annual parameters down to the task's frequency.
 TIME_STEP_IN_YEARS = 60 / (365.25 * 24 * 60 * 60)
 
-class SimulatedMarket:
 
-    def update_stock_prices(self, stocks):
-        # Simulate price updates for the given stocks
+def update_stock_prices(stocks):
+    # Simulate price updates for the given stocks
 
-        new_stocks_prices_list = []
+    new_stocks_prices_list = []
 
-        for stock in stocks:
-            last_price = stock.get_latest_price()
-            if last_price is None:
-                # If no prior price exists, assign a random starting price between 50 and 250
-                last_price = Decimal(random.uniform(50.0, 250.0)).quantize(Decimal('0.0001'))
+    for stock in stocks:
+        last_price = stock.get_latest_price()
+        if last_price is None:
+            # If no prior price exists, assign a random starting price between 50 and 250
+            last_price = Decimal(random.uniform(50.0, 250.0)).quantize(Decimal('0.0001'))
 
 
-             # --- Geometric Brownian Motion Calculation ---
-            drift = (MU - 0.5 * SIGMA**2) * TIME_STEP_IN_YEARS
-            shock = SIGMA * sqrt(TIME_STEP_IN_YEARS) * random.gauss(0, 1)
+            # --- Geometric Brownian Motion Calculation ---
+        drift = (MU - 0.5 * SIGMA**2) * TIME_STEP_IN_YEARS
+        shock = SIGMA * sqrt(TIME_STEP_IN_YEARS) * random.gauss(0, 1)
 
-            price_change_factor = exp(drift + shock)
+        price_change_factor = exp(drift + shock)
 
-            new_price = Decimal(last_price * Decimal(price_change_factor)).quantize(Decimal('0.0001'))
+        new_price = Decimal(last_price * Decimal(price_change_factor)).quantize(Decimal('0.0001'))
 
-            new_stocks_prices_list.append(
-                PriceHistory(
-                    asset=stock,
-                    price=new_price,
-                    source='SIMULATION'
-                )
+        new_stocks_prices_list.append(
+            PriceHistory(
+                asset=stock,
+                price=new_price,
+                source='SIMULATION'
             )
+        )
 
-        if new_stocks_prices_list:
-            PriceHistory.objects.bulk_create(new_stocks_prices_list)
-    def update_currency_pair_prices(self, currency_pairs):
-        # Simulate price updates for the given currency pairs
-        for currency_pair in currency_pairs:
-            # Logic to simulate price change
-            pass
+    if new_stocks_prices_list:
+        PriceHistory.objects.bulk_create(new_stocks_prices_list)
+
+def update_currency_pair_prices(currency_pairs):
+    # Simulate price updates for the given currency pairs
+    for currency_pair in currency_pairs:
+        # Logic to simulate price change
+        pass
