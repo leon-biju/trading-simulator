fd2defd4086e01c3b2e5533c5668c85824a769e7
Leon Biju <leonbiju25@gmail.com>
2026-02-10 11:31:25 +0000

    feat: add home currency support to user profiles and registration form

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,9 +4,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Profile
 from django.conf import settings
+    
+from market.models import Currency
+
 
 # Create a profile for a new instance of User 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        home_currency = getattr(instance, '_home_currency', None)
+        if home_currency is None:
+            home_currency = Currency.objects.get(code='USD')
+        Profile.objects.create(user=instance, home_currency=home_currency)
