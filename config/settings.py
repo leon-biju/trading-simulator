25b22e92586ca77ea4b142c205a26b91566ec7a8
Leon Biju <leonbiju25@gmail.com>
2026-04-18 14:51:48 +0100

    fix: production static file serving

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR / "frontend" / "dist"]
 
 # Media files
 MEDIA_URL = 'media/'
